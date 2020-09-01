649cdb1ab33152c02124ad9f413d63b9b85f920c
Karl Mabunga <karlmabunga@yahoo.com>
2020-09-01 15:13:41 -0700

    Complete function based patter and render example data to screen. Working on class based.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,50 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import VideoList from './VideoList.js';
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+// import fakeVideoData from '/spec/data/fakeVideoData.js'
+
+// var App = () => {
+
+//   return (<div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em><Search /></em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><em><VideoPlayer video={exampleVideoData[0]} /></em></h5></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><h5><em><VideoList videos={exampleVideoData} /></em></h5></div>
+//       </div>
+//     </div>
+//   </div>);
+// };
+
+class App extends React.component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (<div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em><Search /></em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5><em><VideoPlayer video={exampleVideoData[0]} /></em></h5></div>
+        </div>
+        <div className="col-md-5">
+          <div><h5><em><VideoList videos={exampleVideoData} /></em></h5></div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    </div >);
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
